6902b16a458a5e9e219126e29ef922f0e802bfaa
Salman23lol <360legntkiller@gmail.com>
2023-09-11 12:47:03 +0500

    assdadas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Todolist from './list';
+import "./index.css"
+import Test from './test';
+function App() {
+  return (
+    <>
+      {/* <Todolist /> */}
+      <Test />
+    </>
+  );
+}
+
+export default App;
